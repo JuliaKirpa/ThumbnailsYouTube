5a672c31303fcd4ec741dbaa387da8fa68fd2b9f
Gipernova <lia_shongmao@mail.ru>
2022-03-10 12:10:16 +0300

    add test download func

--- test/downloader_test.go
+++ test/downloader_test.go
@@ -3,11 +3,14 @@ package test
 import (
 	"ThumbnailsYouTube_/internal"
 	"ThumbnailsYouTube_/pkg/proto"
+	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 	"log"
 	"net"
 	"testing"
+	"time"
 )
 
 func initGRPCServerHTTP2() {
@@ -27,5 +30,22 @@ func initGRPCServerHTTP2() {
 	}()
 }
 func TestDownload(t *testing.T) {
+	initGRPCServerHTTP2()
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := proto.NewThumbnailsClient(conn)
 
+	url := "https://www.youtube.com/watch?v=lCbAt7bp0H4"
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Download(ctx, &wrapperspb.StringValue{Value: url})
+	if err != nil {
+		log.Fatalf("Could not add product: %v", err)
+	}
+	log.Printf("Res %s", r.String())
 }
